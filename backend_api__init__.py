c22c4fdfc89eccf4421324340035a0920cddc0d5
pasantos65 <pasantos01@gmail.com>
2026-04-14 15:38:20 -0400

    fix: remove misplaced files from root directory

--- backend_api__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-graph_bp = Blueprint("graph", __name__)
-simulation_bp = Blueprint("simulation", __name__)
-report_bp = Blueprint("report", __name__)
-
-from . import graph, simulation, report  # noqa: E402, F401
